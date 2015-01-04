8d6563ff869446bb28b6c9f198c8beb2bcad9089
Takeru Komiya <tkomiya24@gmail.com>
2015-01-04 16:44:28 -0500

    Generates new model Review and finishes the migration file.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024163142) do
+ActiveRecord::Schema.define(version: 20150104213545) do
 
   create_table "bassists", force: true do |t|
     t.boolean  "background_vocals", default: false, null: false
@@ -40,6 +40,15 @@ ActiveRecord::Schema.define(version: 20141024163142) do
     t.integer  "experience",        default: 0,     null: false
     t.integer  "rating",            default: 0
     t.string   "proficiency"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.integer  "reviewer_id", null: false
+    t.integer  "reviewee_id", null: false
+    t.integer  "rating",      null: false
+    t.text     "review"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
