8690e38d1f6f9cde8b20ae485ebc21d300ba2eed
Takeru Komiya <takeru.komiya@sap.com>
2015-09-18 15:06:35 -0400

    Adds a script to setup the database

---
+++ database_setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Run this script to setup the database. This should be hooked up with
+#the git pull command to create consistency for the database among the
+#different development environments. However this can only be done on
+#a per-environment basis.
+
+function outputComment {
+	outputPoundLine
+	echo $1
+	outputPoundLine
+}
+
+function outputPoundLine {
+	echo "####################################"
+}
+
+echo "Initializing database..."
+mysql.server start
+mysql -u root -p -e "
+DROP DATABASE IF EXISTS hobby_site_development;
+CREATE DATABASE hobby_site_development;
+GRANT ALL PRIVILEGES ON hobby_site_development.*
+TO 'hobby_site'@'localhost'
+IDENTIFIED BY 'password';
+exit"
+
+outputComment "Dropping and recreating the database"
+rake db:migrate VERSION=0
+rake db:migrate
+
+outputComment "Seeding default data..."
+rake db:seed
