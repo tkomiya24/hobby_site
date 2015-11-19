9e71a94171934b2d076dd2eeb6b7bbb27c2fbd53
Takeru Komiya <takeru.komiya@sap.com>
2015-11-19 13:55:12 -0500

    Code styles and lints

--- db/migrate/20141018202653_create_bassists.rb
+++ db/migrate/20141018202653_create_bassists.rb
@@ -1,16 +1,14 @@
 class CreateBassists < ActiveRecord::Migration
   def change
     create_table :bassists do |t|
-
       t.references :user
-      t.boolean "background_vocals", :null => false, :default => false
-      t.boolean "five_string", :null => false, :default => false
-      t.boolean "six_string", :null => false, :default => false
-      t.integer "experience", :default => 0, :null => false
-      t.integer "rating", :default => 0
-      t.string "proficiency"
+      t.boolean 'background_vocals', null: false, default: false
+      t.boolean 'five_string', null: false, default: false
+      t.boolean 'six_string', null: false, default: false
+      t.integer 'experience', default: 0, null: false
+      t.integer 'rating', default: 0
+      t.string 'proficiency'
       t.timestamps
-
     end
   end
 end
