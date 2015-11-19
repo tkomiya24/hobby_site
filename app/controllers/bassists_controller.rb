987f3d82799c8c1d189b8826acbaffb21376bee0
Takeru Komiya <takeru.komiya@sap.com>
2015-11-19 12:01:40 -0500

    Code styles and lints

--- app/controllers/bassists_controller.rb
+++ app/controllers/bassists_controller.rb
@@ -1,5 +1,4 @@
 class BassistsController < ApplicationController
-
   layout 'main'
   before_action :check_login
 
@@ -8,51 +7,44 @@ class BassistsController < ApplicationController
   end
 
   def create
-
     @bassist = Bassist.new(bassist_params)
     User.find(session[:user_id]).bassist = @bassist
-
     if @bassist.save
-      flash[:notice] = "Changes successful!"
-      redirect_to(:controller => 'users', :action => 'show')
+      flash[:notice] = 'Changes successful!'
+      redirect_to(controller: 'users', action: 'show')
     else
       render('new')
     end
-
   end
 
   def edit
-
     @bassist = User.find(session[:user_id]).bassist
-
   end
 
   def update
-
     @bassist = User.find(session[:user_id]).bassist
     if @bassist.update_attributes(bassist_params)
-      flash[:notice] = "Changes successful!"
-      redirect_to(:controller => 'users', :action => 'show')
+      flash[:notice] = 'Changes successful!'
+      redirect_to(controller: 'users', action: 'show')
     else
       render('edit')
     end
-
   end
 
   def delete
-
   end
 
   def destroy
     bassist = User.find(session[:user_id]).bassist
     bassist.destroy
-    flash[:notice] = "Deletion successful"
+    flash[:notice] = 'Deletion successful'
     back_to_home
   end
 
   private
 
-    def bassist_params
-      return params.require(:bassist).permit(:background_vocals, :six_string, :five_string, :experience, :proficiency)
-    end
+  def bassist_params
+    params.require(:bassist)
+      .permit(:background_vocals, :six_string, :five_string, :experience, :proficiency)
+  end
 end
