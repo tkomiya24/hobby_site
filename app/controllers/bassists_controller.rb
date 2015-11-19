ca464016e560f071a6d299fe53c785c54cf2f5ff
Takeru Komiya <takeru.komiya@sap.com>
2015-11-19 11:59:56 -0500

    Fixes tabbing

--- app/controllers/bassists_controller.rb
+++ app/controllers/bassists_controller.rb
@@ -1,58 +1,58 @@
 class BassistsController < ApplicationController
 
-	layout 'main'
-	before_action :check_login
+  layout 'main'
+  before_action :check_login
 
-	def new
-		@bassist = Bassist.new
-	end
+  def new
+    @bassist = Bassist.new
+  end
 
-	def create
-		
-		@bassist = Bassist.new(bassist_params)
-		User.find(session[:user_id]).bassist = @bassist
+  def create
 
-		if @bassist.save
-			flash[:notice] = "Changes successful!"
-			redirect_to(:controller => 'users', :action => 'show')
-		else
-			render('new')
-		end
+    @bassist = Bassist.new(bassist_params)
+    User.find(session[:user_id]).bassist = @bassist
 
-	end
+    if @bassist.save
+      flash[:notice] = "Changes successful!"
+      redirect_to(:controller => 'users', :action => 'show')
+    else
+      render('new')
+    end
 
-	def edit
-		
-		@bassist = User.find(session[:user_id]).bassist
+  end
 
-	end
+  def edit
 
-	def update
-		
-		@bassist = User.find(session[:user_id]).bassist
-		if @bassist.update_attributes(bassist_params)
-			flash[:notice] = "Changes successful!"
-			redirect_to(:controller => 'users', :action => 'show')
-		else
-			render('edit')
-		end
+    @bassist = User.find(session[:user_id]).bassist
 
-	end
+  end
 
-	def delete
-		
-	end
+  def update
 
-	def destroy
-		bassist = User.find(session[:user_id]).bassist
-		bassist.destroy
-		flash[:notice] = "Deletion successful"
-		back_to_home
-	end
+    @bassist = User.find(session[:user_id]).bassist
+    if @bassist.update_attributes(bassist_params)
+      flash[:notice] = "Changes successful!"
+      redirect_to(:controller => 'users', :action => 'show')
+    else
+      render('edit')
+    end
 
-	private
+  end
 
-		def bassist_params
-			return params.require(:bassist).permit(:background_vocals, :six_string, :five_string, :experience, :proficiency)
-		end
+  def delete
+
+  end
+
+  def destroy
+    bassist = User.find(session[:user_id]).bassist
+    bassist.destroy
+    flash[:notice] = "Deletion successful"
+    back_to_home
+  end
+
+  private
+
+    def bassist_params
+      return params.require(:bassist).permit(:background_vocals, :six_string, :five_string, :experience, :proficiency)
+    end
 end
