2ba83538752fb773269ed1da7bf12cddceddd320
Takeru Komiya <tkomiya@hotmail.com>
2015-11-18 21:54:55 -0500

    Removes tabs

--- app/controllers/singers_controller.rb
+++ app/controllers/singers_controller.rb
@@ -4,42 +4,42 @@ class SingersController < ApplicationController
   before_action :check_login
 
   def new
-  	@singer = Singer.new
+    @singer = Singer.new
   end
 
   def create
-  	@singer = Singer.new(singer_params)
-  	get_user.singer = @singer
+    @singer = Singer.new(singer_params)
+    get_user.singer = @singer
 
-  	if @singer.save
-  		flash[:notice] = "Changes successful"
-  		redirect_to(:controller => 'users', :action => 'show')
-  	else
-  		render('new')
-  	end
+    if @singer.save
+      flash[:notice] = "Changes successful"
+      redirect_to(:controller => 'users', :action => 'show')
+    else
+      render('new')
+    end
 
   end
 
   def edit
-  	@singer = get_user.singer		
+    @singer = get_user.singer
   end
 
   def update
-  	@singer = get_user.singer
+    @singer = get_user.singer
 
-  	if @singer.update_attributes(singer_params)
-  		flash[:notice] = "Changes successful"
-  		redirect_to(:controller => 'users', :action => 'show')
-  	else
-  		render('edit')
-  	end
+    if @singer.update_attributes(singer_params)
+      flash[:notice] = "Changes successful"
+      redirect_to(:controller => 'users', :action => 'show')
+    else
+      render('edit')
+    end
 
   end
 
   private
 
-  	def singer_params
-  	  return params.require(:singer).permit(:background_vocals, :range, :experience, :proficiency)
-  	end
+    def singer_params
+      return params.require(:singer).permit(:background_vocals, :range, :experience, :proficiency)
+    end
 
 end
