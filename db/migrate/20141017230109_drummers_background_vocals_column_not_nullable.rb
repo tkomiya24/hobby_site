9a4252f1c9d56291e9bc983a40b68f7d9332397e
Takeru Komiya <takeru.komiya@sap.com>
2015-11-19 13:54:22 -0500

    Code styles and lints

--- db/migrate/20141017230109_drummers_background_vocals_column_not_nullable.rb
+++ db/migrate/20141017230109_drummers_background_vocals_column_not_nullable.rb
@@ -1,11 +1,9 @@
 class DrummersBackgroundVocalsColumnNotNullable < ActiveRecord::Migration
-
   def up
-
-  	change_column("drummers", "background_vocals", :boolean, :default => false, :null => false)
+    change_column('drummers', 'background_vocals', :boolean, default: false, null: false)
   end
 
   def down
-  	change_column("drummers", "background_vocals", :boolean, :default => false)
+    change_column('drummers', 'background_vocals', :boolean, default: false)
   end
 end
