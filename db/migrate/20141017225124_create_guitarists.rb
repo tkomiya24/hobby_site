503752ea704283b1ffc4aab704f58e327cc18527
Takeru Komiya <takeru.komiya@sap.com>
2015-11-19 13:53:39 -0500

    Code styles and lints

--- db/migrate/20141017225124_create_guitarists.rb
+++ db/migrate/20141017225124_create_guitarists.rb
@@ -1,17 +1,15 @@
 class CreateGuitarists < ActiveRecord::Migration
   def change
     create_table :guitarists do |t|
-
       t.references :user
-      t.boolean "background_vocals", :null => false, :default => false
-      t.string "rhythm_or_lead", :null => false
-      t.integer "experience", :default => 0, :null => false
-      t.integer "rating", :default => 0
-      t.string "proficiency"
+      t.boolean 'background_vocals', null: false, default: false
+      t.string 'rhythm_or_lead', null: false
+      t.integer 'experience', default: 0, null: false
+      t.integer 'rating', default: 0
+      t.string 'proficiency'
       t.timestamps
-
     end
 
-    add_index "guitarists", "user_id"
+    add_index 'guitarists', 'user_id'
   end
 end
