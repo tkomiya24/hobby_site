fb18ac7b8574c46b0c85c8aaa500e9c0eaa0bf3c
Takeru Komiya <takeru.komiya@sap.com>
2015-11-19 13:47:23 -0500

    Code styles and lints

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 takeru = User.create(username: 'tkomiya', first_name: 'Takeru',
-  last_name: 'Komiya', email: 'tkomiya@hotmail.com', city: 'Vancouver',
-  password: 'password');
+                     last_name: 'Komiya', email: 'tkomiya@hotmail.com', city: 'Vancouver',
+                     password: 'password')
 
 takeru.drummer = Drummer.create(background_vocals: false, double_kick: true,
-  experience: 13);
+                                experience: 13)
 
 takeru.guitarist = Guitarist.create(background_vocals: false, experience: 13,
-  rhythm_or_lead: 'Rhythm');
+                                    rhythm_or_lead: 'Rhythm')
