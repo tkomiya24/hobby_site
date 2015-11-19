590cf3177a42743504f53ec85fb932f071cca5be
Takeru Komiya <takeru.komiya@sap.com>
2015-11-19 13:44:15 -0500

    Code styles and lints

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,62 +1,48 @@
 class User < ActiveRecord::Base
-
-  #security
+  # security
   has_secure_password
-  #validates :password_confirmation, :presence => true
+  # validates :password_confirmation, :presence => true
 
-  #validations
+  # validations
   EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.0-]+\.[a-z]{2,4}\Z/i
-  validates :username, :uniqueness => true, :presence => true
-  validates :email, :presence => true, :format => EMAIL_REGEX, :confirmation => true
+  validates :username, uniqueness: true, presence: true
+  validates :email, presence: true, format: EMAIL_REGEX, confirmation: true
 
-  #associations
-=begin
-  has_one :drummer
-  has_one :guitarist
-  has_one :bassist
-=end
+  # associations
   belongs_to :drummer
   belongs_to :guitarist
   belongs_to :bassist
   belongs_to :singer
-  has_many :received_reviews, :class_name => "Review", :foreign_key => "reviewee_id"
-  has_many :written_reviews, :class_name => "Review", :foreign_key => "reviewer_id"
+  has_many :received_reviews, class_name: 'Review', foreign_key: 'reviewee_id'
+  has_many :written_reviews, class_name: 'Review', foreign_key: 'reviewee_id'
 
   def get_matches(limit)
-    return User.where(["CITY = ? AND (drummer_id IS NOT NULL OR guitarist_id IS NOT NULL OR bassist_id IS NOT NULL)", self.city]).
-    where.not(:id => self.id)
+    User.where(['CITY = ? AND '\
+                '(drummer_id IS NOT NULL OR '\
+                'guitarist_id IS NOT NULL OR '\
+                'bassist_id IS NOT NULL)', city]).where.not(id: id).limit(limit)
   end
 
   def get_drummers(limit)
-    return User.where(:city => self.city).where.not(:id => self.id, :drummer_id => nil).limit(limit)
+    User.where(city: city).where.not(id: id, drummer_id: nil).limit(limit)
   end
 
   def get_guitarists(limit)
-    return User.where(:city => self.city).where.not(:id => self.id, :guitarist_id => nil).limit(limit)
+    User.where(city: city).where.not(id: id, guitarist_id: nil).limit(limit)
   end
 
   def get_bassists(limit)
-    return User.where(:city => self.city).where.not(:id => self.id, :bassist_id => nil).limit(limit)
+    User.where(city: city).where.not(id: id, bassist_id: nil).limit(limit)
   end
 
   def musician?
-    return self.drummer != nil || self.guitarist != nil || self.bassist != nil || self.singer != nil
+    !drummer.nil? || !guitarist.nil? || !bassist.nil? || !singer.nil?
   end
 
   def nullify_deleted
-
-    unless self.drummer != nil
-      self.drummer_id = nil
-    end
-
-    unless self.bassist != nil
-      self.bassist_id = nil
-    end
-
-    unless self.guitarist != nil
-      self.guitarist_id = nil
-    end
-
-    self.save
+    drummer_id = nil if drummer.nil?
+    bassist_id = nil if bassist.nil?
+    guitarist_id = nil if guitarist.nil?
+    save
   end
 end
