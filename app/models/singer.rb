930b7654c6b27e2fbd30119041d1c02b3742b4c9
Takeru Komiya <takeru.komiya@sap.com>
2015-11-20 11:15:26 -0500

    Splits long line

--- app/models/singer.rb
+++ app/models/singer.rb
@@ -2,6 +2,7 @@ class Singer < ActiveRecord::Base
   has_one :user, dependent: :nullify
 
   # validations
-  validates :proficiency, inclusion: { in: [nil, 'Beginner', 'Intermmediate', 'Advanced', 'Professional'] }
+  validates :proficiency,
+            inclusion: { in: [nil, 'Beginner', 'Intermmediate', 'Advanced', 'Professional'] }
   validates :range, inclusion: { in: %w(Bass Baritone Tenor Alto Soprano) }
 end
