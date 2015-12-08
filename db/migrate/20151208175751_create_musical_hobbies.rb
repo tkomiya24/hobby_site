b818715663bb22f74e934d51cb0d374cb4267582
Takeru Komiya <takeru.komiya@sap.com>
2015-12-08 13:29:49 -0500

    Changes syntax

--- db/migrate/20151208175751_create_musical_hobbies.rb
+++ db/migrate/20151208175751_create_musical_hobbies.rb
@@ -2,9 +2,8 @@ class CreateMusicalHobbies < ActiveRecord::Migration
   def up
     create_table :musical_hobbies do |t|
       t.timestamps null: false
-      t.references :user
+      t.references :user, index: true
     end
-    add_index :musical_hobbies, :user_id
   end
 
   def down
