8f1e66aac9528fb88743d882672f8fddac903e70
Takeru Komiya <tkomiya@hotmail.com>
2015-12-08 20:29:56 -0500

    Removes a validation

--- app/models/singer.rb
+++ app/models/singer.rb
@@ -1,8 +1,4 @@
 class Singer < ActiveRecord::Base
   has_one :user, dependent: :nullify
-
-  # validations
-  validates :proficiency,
-            inclusion: { in: [nil, 'Beginner', 'Intermmediate', 'Advanced', 'Professional'] }
   validates :range, inclusion: { in: %w(Bass Baritone Tenor Alto Soprano) }
 end
