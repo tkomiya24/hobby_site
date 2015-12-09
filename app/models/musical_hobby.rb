b60f9f2e98154744cc3817afdf1c8e80d59e1602
Takeru Komiya <tkomiya@hotmail.com>
2015-12-08 20:29:29 -0500

    Adds a validation

--- app/models/musical_hobby.rb
+++ app/models/musical_hobby.rb
@@ -1,4 +1,6 @@
 class MusicalHobby < ActiveRecord::Base
   belongs_to :user
   belongs_to :instrument, polymorphic: true
+  validates :proficiency,
+            inclusion: { in: [nil, 'Beginner', 'Intermmediate', 'Advanced', 'Professional'] }
 end
