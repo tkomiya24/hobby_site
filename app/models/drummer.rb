8850e08a21a3e2a8c30fc6869a74a83af758d0ce
Takeru Komiya <tkomiya@hotmail.com>
2015-12-08 19:07:44 -0500

    Allows for nested forms

--- app/models/drummer.rb
+++ app/models/drummer.rb
@@ -1,4 +1,5 @@
 class Drummer < ActiveRecord::Base
   has_one :musical_hobby, as: :instrument, dependent: :destroy
   has_one :user, through: :musical_hobby
+  accepts_nested_attributes_for :musical_hobby
 end
