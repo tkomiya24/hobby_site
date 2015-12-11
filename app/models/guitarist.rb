b731bef44d0bf7bbf81de9259514b31aa00da7dc
Takeru Komiya <takeru.komiya@sap.com>
2015-12-11 14:06:49 -0500

    Fixes edit to not make a new musical_hobby

--- app/models/guitarist.rb
+++ app/models/guitarist.rb
@@ -1,6 +1,6 @@
 class Guitarist < ActiveRecord::Base
   has_one :musical_hobby, as: :instrument, dependent: :destroy
   has_one :user, through: :musical_hobby
-  accepts_nested_attributes_for :musical_hobby
+  accepts_nested_attributes_for :musical_hobby, update_only: true
   validates :rhythm_or_lead, inclusion: { in: %w(Rhythm Lead Both) }
 end
