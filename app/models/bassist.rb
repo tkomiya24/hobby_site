9380d31d43a13e681f72b253155e6d756c9ee70f
Takeru Komiya <tkomiya@hotmail.com>
2015-12-10 19:48:23 -0500

    updates bassist model

--- app/models/bassist.rb
+++ app/models/bassist.rb
@@ -1,10 +1,5 @@
 class Bassist < ActiveRecord::Base
-  # associations
-  # belongs_to :user
-  has_one :user, dependent: :nullify
-
-  # validations
-  # validates :user_id, :presence => true
-  validates :proficiency,
-            inclusion: { in: [nil, 'Beginner', 'Intermmediate', 'Advanced', 'Professional'] }
+  has_one :musical_hobby, as: :instrument, dependent: :destroy
+  has_one :user, through: :musical_hobby
+  accepts_nested_attributes_for :musical_hobby
 end
