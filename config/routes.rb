d96e68696b1e8d2c12f6b75b536ec34672ab3e2a
Takeru Komiya <tkomiya@hotmail.com>
2016-02-04 16:25:59 -0800

    Adds a resources to reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'users/search/:query' => 'users#search'
-  match ':controller(/:action(/:id))', via: [:get, :post, :delete, :patch]
+  resources :reviews
+  match ':controller(/:action(/:id))', via: [:get, :post, :delete]
 end
