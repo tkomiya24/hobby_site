2bc239ce2d89522fdd1b1d8b61b3146af530e00f
Takeru Komiya <tkomiya@hotmail.com>
2016-02-06 11:37:56 -0800

    Fixes user relations and removes unneeded methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,70 +8,8 @@ class User < ActiveRecord::Base
   validates :username, uniqueness: true, presence: true
   validates :email, presence: true, format: EMAIL_REGEX, confirmation: true
   has_many :written_reviews, class_name: 'Review'
-  has_many :musical_hobbies
-
-  def find_matches
-    User.where(city: city).where.not(id: id).limit(100)
-  end
-
-  def guitarist?
-    plays_instrument?('Guitarist')
-  end
-
-  def singer?
-    plays_instrument?('Singer')
-  end
-
-  def drummer?
-    plays_instrument?('Drummer')
-  end
-
-  def bassist?
-    plays_instrument?('Bassist')
-  end
-
-  def plays_instrument?(instrument)
-    musical_hobbies.each do |hobby|
-      return true if hobby.instrument_type == instrument
-    end
-    false
-  end
-
-  def drummer
-    instrument('Drummer')
-  end
-
-  def singer
-    instrument('Singer')
-  end
-
-  def bassist
-    instrument('Bassist')
-  end
-
-  def guitarist
-    instrument('Guitarist')
-  end
-
-  def instrument(instrument)
-    musical_hobbies.each do |hobby|
-      return hobby.instrument if hobby.instrument_type == instrument
-    end
-  end
-
-  def get_drummers(limit)
-    User.where(city: city).where.not(id: id, drummer_id: nil).limit(limit)
-  end
-
-  def get_guitarists(limit)
-    User.where(city: city).where.not(id: id, guitarist_id: nil).limit(limit)
-  end
-
-  def get_bassists(limit)
-    User.where(city: city).where.not(id: id, bassist_id: nil).limit(limit)
-  end
-
-  def musician?
-    musical_hobbies.size > 1
-  end
+  has_one :bassist, dependent: :destroy
+  has_one :guitarist, dependent: :destroy
+  has_one :drummer, dependent: :destroy
+  has_one :singer, dependent: :destroy
 end
