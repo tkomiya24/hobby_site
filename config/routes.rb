743f6e4dfd44b11ff180d5d36dfd8308cb7232d5
Takeru Komiya <tkomiya@hotmail.com>
2016-02-06 20:06:19 -0800

    Fixes match in route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'users/search/:query' => 'users#search'
   resources :reviews
-  match ':controller(/:action(/:id))', via: [:get, :post, :delete]
+  match ':controller(/:action(/:id))', via: [:get, :post, :delete, :patch]
 end
