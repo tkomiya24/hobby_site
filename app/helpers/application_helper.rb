0a934c53404d5b6947ffea9c360d7c2100470ab3
Takeru Komiya <tkomiya@hotmail.com>
2016-02-07 11:38:00 -0800

    Switches review create to view if review exists

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,14 @@ module ApplicationHelper
   end
 
   def make_review_link_for(user, musician)
-    link_to 'Write a review', new_musician_review_path(musician) unless authenticated_user?(user)
+    return if authenticated_user?(user)
+    authenticated_user = User.find(session[:user_id])
+    if authenticated_user.reviewed_musician?(musician)
+      link_to 'View your review',
+              musician_review_path(musician, musician.review_from(authenticated_user))
+    else
+      link_to 'Write a review', new_musician_review_path(musician)
+    end
   end
 
   def make_show_reviews_link_for(musician)
--- app/models/musician.rb
+++ app/models/musician.rb
@@ -4,4 +4,8 @@ class Musician < ActiveRecord::Base
   validates :proficiency,
             inclusion: { in: [nil, 'Beginner', 'Intermmediate', 'Advanced', 'Professional'] }
   has_many :reviews, as: :reviewable, dependent: :delete_all
+
+  def review_from(user)
+    reviews.find { |review| review.user == user }
+  end
 end
