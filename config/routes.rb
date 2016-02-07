80a8b58258eee40e7d11c4e5b2ae66e9d987ed5f
Takeru Komiya <tkomiya@hotmail.com>
2016-02-07 09:46:41 -0800

    Adds musicians as a resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'users/search/:query' => 'users#search'
+  resources :musicians do
+    resources :reviews
+  end
   resources :reviews
   match ':controller(/:action(/:id))', via: [:get, :post, :delete, :patch]
 end
