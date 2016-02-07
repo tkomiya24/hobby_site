f7830920c58da2f4e304b19133519958acec16fa
Takeru Komiya <tkomiya@hotmail.com>
2016-02-07 09:58:11 -0800

    Adds two new helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def error_messages_for(object)
     render(partial: 'application/error_message', locals: { object: object })
   end
+
+  def authenticated_user?(user)
+    user.id == session[:id]
+  end
+
+  def make_review_link_for(user, musician)
+    link_to 'Write a review', new_musician_review_path(musician) unless authenticated_user?(user)
+  end
 end
