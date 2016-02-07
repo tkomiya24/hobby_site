e5871adcf7d900fc504290546d23555108cff7ba
Takeru Komiya <tkomiya@hotmail.com>
2016-02-07 10:02:03 -0800

    Implements new method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,5 +2,7 @@ class ReviewsController < ApplicationController
   layout 'main'
 
   def new
+    @review = Review.new
+    @review.reviewable = Musician.find(params[:musician_id]).specific
   end
 end
