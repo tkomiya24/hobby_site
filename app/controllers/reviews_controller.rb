0848ac338fa607cf113eca6e38b75ce63afad029
Takeru Komiya <tkomiya@hotmail.com>
2016-02-07 10:17:59 -0800

    Implements create method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,4 +5,15 @@ class ReviewsController < ApplicationController
     @review = Review.new
     @musician = Musician.find(params[:musician_id]).specific
   end
+
+  def create
+    @review = Review.new(params.require(:review).permit(:rating, :review))
+    @review.user = fetch_user
+    @review.reviewable = Musician.find(params[:musician_id]).specific
+    if @review.save
+      redirect_to_user
+    else
+      render('new')
+    end
+  end
 end
