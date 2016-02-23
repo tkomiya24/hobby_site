8dbe35ccbe84645767a15e9ff3d1c691d26f8410
Takeru Komiya <tkomiya@hotmail.com>
2016-02-23 12:54:37 -0800

    Adds helper methods to create links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,28 @@ module ApplicationHelper
   def make_show_reviews_link_for(musician)
     link_to 'See the reviews', musician_reviews_path(musician)
   end
+
+  def make_edit_musician_link(musician, options = nil)
+    type = musician.specific.class.name
+    case type
+    when 'Drummer'
+      edit_link_helper(:drummers, options)
+    when 'Guitarist'
+      edit_link_helper(:guitarists, options)
+    when 'Bassist'
+      edit_link_helper(:bassists, options)
+    when 'Singer'
+      edit_link_helper(:singers, options)
+    end
+  end
+
+  private
+
+  def edit_link_helper(instrument, html_options)
+    link_to_helper('Edit', { controller: instrument, action: :edit }, html_options)
+  end
+
+  def link_to_helper(body, url_options, html_options)
+    link_to(body, url_options, html_options)
+  end
 end
