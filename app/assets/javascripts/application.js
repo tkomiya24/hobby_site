7f6926867a1062aea561da13e9afc1ac467d0562
Takeru Komiya <tkomiya@hotmail.com>
2016-03-01 09:44:25 -0800

    Adds jquery_ujs back to asset pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//a= require jquery_ujs
+//= require jquery_ujs
 //a= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
