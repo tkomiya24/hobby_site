1e6328f6c2e079d29ea9ee577e58c4b017f3bfb9
Takeru Komiya <tkomiya@hotmail.com>
2016-03-02 13:27:35 -0800

    Adds a validation for uniqueness of instrument to user

--- app/models/musician.rb
+++ app/models/musician.rb
@@ -5,7 +5,9 @@ class Musician < ActiveRecord::Base
             inclusion: { in: [nil, 'Beginner', 'Intermmediate', 'Advanced', 'Professional'] }
   has_many :reviews, as: :reviewable, dependent: :delete_all
   has_many :reviewers, class_name: 'User', through: :reviews
-  validates :actable_type, uniqueness: { scope: :user_id }
+  validates :actable_type,
+            uniqueness: { scope: :user_id,
+                          message: 'has already been added' }
 
   def review_from(user)
     reviews.find { |review| review.user == user }
