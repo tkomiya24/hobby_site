9c404754e93eb07f00b7ed4b570dbfaf65577df8
Takeru Komiya <tkomiya@hotmail.com>
2016-03-08 14:37:21 -0800

    Adds an index method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,13 @@ class UsersController < ApplicationController
   before_action :fetch_user, except: [:login, :new, :create, :attempt_login, :logout]
 
   # CRUD actions
+  def index
+    @users = []
+    User.where.not(id: @user.id).find_each do |user|
+      @users.push(user)
+    end
+  end
+
   def new
     @user = User.new
   end
