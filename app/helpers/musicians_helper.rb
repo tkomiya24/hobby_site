ba0a014b7cb68ec94e2ecec54773ddb7aa905257
Takeru Komiya <tkomiya@hotmail.com>
2016-03-09 10:51:54 -0800

    Adds a helper method to render appropriate summary

--- app/helpers/musicians_helper.rb
+++ app/helpers/musicians_helper.rb
@@ -2,4 +2,9 @@ module MusiciansHelper
   def new_musician_instrument(title, instrument)
     link_to(title, controller: 'musicians', action: 'new', instrument: instrument)
   end
+
+  def render_specific_summary(musician)
+    render(partial: "#{symbol_from_musician(musician)}/summary",
+           locals: { musician: musician.specific })
+  end
 end
