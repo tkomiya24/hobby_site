b01f07a6b25d5b053f479432d68fcea3be1dd6b6
Takeru Komiya <tkomiya@hotmail.com>
2016-03-09 10:51:31 -0800

    Adds a method to print summary

--- app/models/guitarist.rb
+++ app/models/guitarist.rb
@@ -1,4 +1,15 @@
 class Guitarist < ActiveRecord::Base
   validates :rhythm_or_lead, inclusion: { in: %w(Rhythm Lead Both) }
   acts_as :musician
+
+  def type_summary
+    case rhythm_or_lead
+    when 'Rhythm'
+      'Plays rhythm guitar'
+    when 'Lead'
+      'Plays lead guitar'
+    when 'Both'
+      'Plays both lead and rhythm guitar'
+    end
+  end
 end
