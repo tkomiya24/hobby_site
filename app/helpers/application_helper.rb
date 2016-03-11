8ebe2b85197f693ca46155d5e02efe985148860b
Takeru Komiya <tkomiya@hotmail.com>
2016-03-11 15:31:59 -0800

    Moves a helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,18 +7,6 @@ module ApplicationHelper
     user.id == session[:user_id]
   end
 
-  def make_review_link_for(user, musician, options)
-    return if authenticated_user?(user)
-    authenticated_user = User.find(session[:user_id])
-    if authenticated_user.reviewed_musician?(musician)
-      link_to('View your review',
-              musician_review_path(musician, musician.review_from(authenticated_user)),
-              options)
-    else
-      link_to 'Write a review', new_musician_review_path(musician)
-    end
-  end
-
   def make_edit_musician_link(musician, options = nil)
     edit_link_helper(symbol_from_musician(musician), options)
   end
--- app/helpers/musicians_helper.rb
+++ app/helpers/musicians_helper.rb
@@ -7,4 +7,16 @@ module MusiciansHelper
     render(partial: "#{symbol_from_musician(musician)}/summary",
            locals: { musician: musician.specific })
   end
+
+  def make_review_link_for(user, musician, options)
+    return if authenticated_user?(user)
+    authenticated_user = User.find(session[:user_id])
+    if authenticated_user.reviewed_musician?(musician)
+      link_to('View your review',
+              musician_review_path(musician, musician.review_from(authenticated_user)),
+              options)
+    else
+      link_to 'Write a review', new_musician_review_path(musician)
+    end
+  end
 end
