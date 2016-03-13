1b2e80bfafd274e284ba0e898515b8f53fa02590
Takeru Komiya <tkomiya@hotmail.com>
2016-03-13 11:08:22 -0700

    Adds an extra user

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,15 @@ ActiveRecord::Base.connection.tables.each do |table|
   # ActiveRecord::Base.connection.execute("DELETE FROM #{table}")
 end
 
-takeru = User.create(username: 'tkomiya', first_name: 'Takeru',
-                     last_name: 'Komiya', email: 'tkomiya@fake.com', city: 'Vancouver',
-                     password: 'password')
-
+takeru = User.create(username: 'tkomiya24', city: 'Vancouver', password: 'password')
 takeru.musicians << Guitarist.new(background_vocals: false, experience: 13,
-                                  rhythm_or_lead: 'Rhythm')
+                                  rhythm_or_lead: 'Rhythm', proficiency: 'Intermmediate')
+takeru.musicians << Drummer.new(background_vocals: true, experience: 15, double_kick: true,
+                                proficiency: 'Advanced')
 
-jared = User.create(username: 'jzecchel', first_name: 'Jared',
-                    last_name: 'Zecchel', email: 'jzecchel@fake.com', city: 'Vancouver',
-                    password: 'password')
+jared = User.create(username: 'jz123', city: 'Vancouver', password: 'password')
+jared.musicians << Bassist.new(background_vocals: true, experience: 13, proficiency: 'Advanced')
 
-jared.musicians << Bassist.new(background_vocals: true, experience: 13)
+nick = User.create(username: 'nlan23', city: 'Vancouver', password: 'password')
+nick.musicians << Guitarist.new(background_vocals: true, experience: 13,
+                                rhythm_or_lead: 'Rhythm', proficiency: 'Advanced')
